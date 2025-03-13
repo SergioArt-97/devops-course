8abb40cc6f20a26c51f07b9c1de90fd2056ff821
SergioArt-97 <sergeiart97@gmail.com>
2025-03-13 23:23:28 +0200

    Lesson_9_HW

---
+++ Lesson_9/flask_docker_build/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/new')
+def new():
+    return "New Route"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
